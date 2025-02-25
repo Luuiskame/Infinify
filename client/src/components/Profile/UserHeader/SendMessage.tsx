d27465d117d33cd8b098f5036471f7dcacb5d0d4
Luis Reyes <yuiftw05@gmail.com>
2025-02-25 06:32:35 -0600

    feat: adding chat finder function to our send message to find the chat locally before dispatching the result

--- client/src/components/Profile/UserHeader/SendMessage.tsx
+++ client/src/components/Profile/UserHeader/SendMessage.tsx
@@ -3,7 +3,7 @@ import { useCreateOrFindChatMutation } from '@/services/chatsApi'
 import { useRouter } from 'next/navigation'
 import { setOneChat } from '@/slices/chatSlice'
 import { useAppDispatch } from '@/redux/hooks'
-
+import { useAppSelector } from '@/redux/hooks'
 interface chatUsersInfo {
     localUser: string
     profileUser: string
@@ -13,11 +13,18 @@ interface chatUsersInfo {
 export default function SendMessage(chatInfo: chatUsersInfo) {
     const dispatch = useAppDispatch()
     const router = useRouter()
+    const chats = useAppSelector((state)=> state.chatsReducer.user_chats)
     const [createOrFindChat,{isLoading, isError, data}] = useCreateOrFindChatMutation()
 
     //! function to look for the avalaible chat before doing the req to our backend
     
-    
+    const chatFinder = (chatId: string) => {
+      const chatFound = chats?.find(chat=> chat.chatInfo.id === chatId)
+
+      if(chatFound?.chatInfo.id){
+        return chatFound.chatInfo.id
+      }
+    }
 
     const trigger = async ()=> {
         const chatPayload = {
@@ -27,16 +34,23 @@ export default function SendMessage(chatInfo: chatUsersInfo) {
         const result = await createOrFindChat(chatPayload).unwrap()
         console.log(result)
 
-        const completedProperties = {
-            ...result,
-            chat_messages: [],
-            isFetched: false
-        }
-        
-        if(result?.chatInfo.id){
+        if(result.chatInfo.id){
+          const localChatExistAlready = chatFinder(result.chatInfo.id)
+
+          if(localChatExistAlready !== undefined){
+            router.push(`/chats/${localChatExistAlready}`)
+          } else {
+            const completedProperties = {
+              ...result,
+              chat_messages: [],
+              isFetched: false
+          }
             dispatch(setOneChat(completedProperties))
             router.push(`/chats/${result.chatInfo.id}`)
+            
+          }
         }
+        
 
     }
   return (
