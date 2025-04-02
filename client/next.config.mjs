ee1deceb4e24eda78057f93c44b596e49b0222d1
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-04-01 21:06:49 -0300

    refactor: eliminar opción experimental de optimización de CSS en next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizeCss: true, // Reduce el CSS automáticamente
-  },
   images: {
     remotePatterns: [
       {
